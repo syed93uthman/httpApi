7760722bb75058b092408723fa04bd9dfd49c32b
syed93uthman <syed_uthman@yahoo.com>
2024-11-18 15:12:42 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var app = express();
+const flightsInfo = require('./mockFlightsInfo.json');
+
+app.get('/flights', function (req, res) {
+    var data = flightsInfo;
+    
+    data.flights = data.flights.map(flight => flight.flightId);
+    console.log(data);
+
+    res.send(data);
+    //res.end();
+})
+
+app.get('/flight/:flightId/info', function (req, res) {
+    var success = false;
+    var data = flightsInfo;
+    const flightId = req.params.flightId;
+
+
+    var flight = data.flights.find(function (element) {
+        if (element.flightId == flightId) {
+            success = true;
+            return element;
+        }
+    })
+
+    if (success) {
+        delete flight.flightId;
+        console.log(flight);
+        res.send(flight);
+    }
+    else {
+        console.log("Flight not found");
+        res.send("Flight not found");
+    }
+
+    //res.end();
+})
+
+
+var server = app.listen(3000, function () {
+    console.log("Flights Info Server running at port 3000/");
+})
